9405a71a216b86685340107cf7332231b78f01c1
Mark Ambrocio <wowmarkambro@gmail.com>
2023-11-26 12:00:44 -0700

    more seed users

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -6,19 +6,40 @@ import {
 	syncStripeProducts
 } from './utils';
 
+const testUsers = [
+	{
+		full_name: 'Test User 1',
+		email: 't1@t.com',
+		password: 'password'
+	},
+	{
+		full_name: 'Test User 2',
+		email: 't2@t.com',
+		password: 'password'
+	},
+	{
+		full_name: 'Test User 3',
+		email: 't3@t.com',
+		password: 'password'
+	}
+];
+
 async function seed() {
 	try {
 		await startSupabase();
 		await clearSupabaseData();
 		await syncStripeProducts();
-		const user = await createUser({
+		await createUser({
 			email: 'mark@me.com',
 			full_name: 'Mark Test User',
 			password: 'password'
 		});
 
-		for (let i = 0; i < 5; i++) {
-			await createContact(user.id);
+		for (const testUser of testUsers) {
+			const user = await createUser(testUser);
+			for (let i = 0; i < 5; i++) {
+				await createContact(user.id);
+			}
 		}
 	} catch (err) {
 		console.error(err);
